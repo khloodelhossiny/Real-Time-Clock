3b836bd9fcd77c6a51a9727cbeef39751206437a
KhloodElhossiny <47139708+khloodelhossiny@users.noreply.github.com>
2023-04-26 14:41:10 +0200

    Add files via upload

---
+++ clock-sys/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+External_Interrupts.c
+
+lcd.c
+
+main.c
+
+Seven_Seg.c
+
+Timer0.c
+
